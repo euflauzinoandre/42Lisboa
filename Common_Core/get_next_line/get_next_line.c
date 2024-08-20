7d3c1ddbc817f89b0724fa46cc64bf408467566e
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2024-08-20 16:41:54 +0100

    Update get_next_line.c

--- Common_Core/get_next_line/get_next_line.c
+++ Common_Core/get_next_line/get_next_line.c
@@ -5,46 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/16 17:32:04 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/07/16 22:35:58 by aeuflauz         ###   ########.fr       */
+/*   Created: 2024/06/17 13:26:01 by aeuflauz          #+#    #+#             */
+/*   Updated: 2024/08/20 16:35:08 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*create_line(int fd, char *buffer)
-{
-	static char	*string;
-	size_t		bytes;
-	size_t		i;
-	size_t		size_line;
-
-	size_line = calc_size_line(fd, buffer);
-	string = malloc(size_line + 1);
-	if (!string)
-		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, BUFFER_SIZE) < 0)
-		return (NULL);
-	while ((bytes = read(fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		buffer[bytes] = '\0';
-		i = 0;
-		while (i < bytes)
-		{
-			if (buffer[i] == '\n')
-				append(string, buffer);
-		}
-		append(string, buffer);
-	}
-	return (string);
-}
-
 char	*get_next_line(int fd)
 {
-	char	buffer[BUFFER_SIZE + 1];
-	char	*get_line;
+	static char	*remainder;
+	char		*get_line;
+	char		buffer[BUFFER_SIZE + 1];
 
-	get_line = create_line(fd, buffer);
-	clear(string);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!remainder)
+		remainder = read_file(fd, buffer);
+	if (remainder)
+	{
+		get_line = ft_get_line (remainder);
+		remainder = ft_get_rest (remainder);
+	}
 	return (get_line);
 }
