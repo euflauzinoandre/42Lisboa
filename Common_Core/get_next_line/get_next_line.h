b50c9f81f9becdc2655418447952ba9df1e66b36
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2024-08-20 16:42:49 +0100

    Update get_next_line.h

--- Common_Core/get_next_line/get_next_line.h
+++ Common_Core/get_next_line/get_next_line.h
@@ -5,28 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/16 17:25:54 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/07/30 12:02:27 by aeuflauz         ###   ########.fr       */
+/*   Created: 2024/06/17 13:27:00 by aeuflauz          #+#    #+#             */
+/*   Updated: 2024/08/20 16:42:38 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-# include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-# include <fcntl.h>
 
 char	*get_next_line(int fd);
-size_t	size_line(int fd, char *buffer);
-void	append(char *dest, const char *src);
-char	*create_line(int fd, char *buffer);
-void	clear(char *string);
+...
 
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif //BUFFER_SIZE
+#endif //GET_NEXT_LINE_H
