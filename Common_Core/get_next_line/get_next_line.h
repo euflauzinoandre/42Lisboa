5e29d8f6c8652382780d80ece4af87dc638f7a12
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2024-08-27 17:05:15 +0100

    Update get_next_line.h

--- Common_Core/get_next_line/get_next_line.h
+++ Common_Core/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 13:27:00 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/08/20 16:42:38 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/08/27 16:59:19 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include <string.h>
+# include <fcntl.h> 
 
 char	*get_next_line(int fd);
-...
+char	*read_file(int fd, char *str);
+char	*ft_get_line(char *str);
+char	*ft_get_rest(char *str);
+char	*find_newline(char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
