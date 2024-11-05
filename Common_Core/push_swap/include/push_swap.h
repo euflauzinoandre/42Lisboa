a471011369c37f9a31f7d1a82a5ae92057e39104
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2024-11-05 16:28:26 +0000

    Update push_swap.h

--- Common_Core/push_swap/include/push_swap.h
+++ Common_Core/push_swap/include/push_swap.h
@@ -6,15 +6,13 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:37:21 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/10/15 15:05:49 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:24:04 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-# include "../libft/header/libft.h"
-
-
+# include "../libft/include/libft.h"
 
 #endif //PUSH_SWAP_H
