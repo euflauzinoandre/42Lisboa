84826dec9fcf95aef01e4594a1dba231ea6c8571
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2024-11-12 14:36:20 +0000

    Update push_swap.c

--- Common_Core/push_swap/src/push_swap.c
+++ Common_Core/push_swap/src/push_swap.c
@@ -6,9 +6,22 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:37:15 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/11/05 14:26:08 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:26:20 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+int	main(int argc, char **argv)
+{
+	int	*numbers;
+
+	if (argc < 2)
+		return (0);
+	numbers = create_list(argc, argv);
+	ft_printf("ARGC = %d\n", argc);
+	while (*numbers)
+		ft_printf("%d\n", *numbers++);
+	free(numbers);
+	return (0);
+}
