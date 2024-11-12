263a56a3749d84e2760c436ad5719b68a6355185
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2024-11-12 14:35:22 +0000

    Update push_swap.h

--- Common_Core/push_swap/include/push_swap.h
+++ Common_Core/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:37:21 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/11/05 14:24:04 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:07:02 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 # define PUSH_SWAP_H
 
 # include "../libft/include/libft.h"
+
+// /* Operations */
+// void	swap(t_stack **stack);
+// void	push(t_stack **from, t_stack **to);
+// void	rotate(t_stack **stack);
+// void	reverse_rotate(t_stack **stack);
+
+/* Utils */
+void	ft_free(char **str);
+int	*create_list(int argc, char **argv);
+
+/* Stack */
+typedef struct s_stack
+{
+	int			*value;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
 
 #endif //PUSH_SWAP_H
