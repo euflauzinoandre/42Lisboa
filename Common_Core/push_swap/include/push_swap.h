09c43bc50531a5f8fc3448fe0c5233d7bd6c0a66
André Euflauzino <andre.euflauzino@gmail.com>
2024-11-18 14:59:41 +0000

    update header push_swap

--- Common_Core/push_swap/include/push_swap.h
+++ Common_Core/push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:37:21 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/11/16 19:21:54 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:49:55 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,20 @@
 # include "../libft/include/libft.h"
 
 // /* Operations */
-// void	swap(t_stack **stack);
-// void	push(t_stack **from, t_stack **to);
-// void	rotate(t_stack **stack);
-// void	reverse_rotate(t_stack **stack);
+// void	swap(t_list **stack);
+// void	push(t_list **from, t_list **to);
+// void	rotate(t_list **stack);
+// void	reverse_rotate(t_list **stack);
 
 /* Tools */
 void	ft_free(char **str);
-int	total_size(char **arr);
+int	count_numbers(int *numbers);
 
 /* Utils */
-int	*two_argument(char **argv);
-int	*many_arguments(char **argv);
 
-/* Stack */
-typedef struct s_stack
-{
-	int			*value;
-	struct s_stack	*next;
-	struct s_stack	*prev;
-}	t_stack;
+/* Init Stack */
+void	init_stack(t_list **stack, int argc, char **argv);
+void	print_stack(char *str, t_list *stack_a);
+// void	sort_list(t_list **stack_a, t_list **stack_b);
 
 #endif //PUSH_SWAP_H
