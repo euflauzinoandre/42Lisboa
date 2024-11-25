720b4fca057d12064cf9d6ecb8e7ff0783237296
André Euflauzino <andre.euflauzino@gmail.com>
2024-11-25 16:23:38 +0000

    Create rotate.c

---
+++ Common_Core/push_swap/src/rotate.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 16:01:37 by aeuflauz          #+#    #+#             */
+/*   Updated: 2024/11/25 16:17:56 by aeuflauz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+int	rotate(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	*stack = head->next;
+	tail->next = head;
+	return (0);
+}
+
+int	ra(t_list **stack_a)
+{
+	if (rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("ra", 1);
+	return (0);
+}
+
+int	rb(t_list **stack_b)
+{
+	if (rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rb", 1);
+	return (0);
+}
+
+int	rr(t_list **stack_a, t_list **stack_b)
+{
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
+	ft_putendl_fd("rr", 1);
+	return (0);
+}
