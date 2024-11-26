829a2abf77ab415274a1a24080724a914c3a4cff
André Euflauzino <andre.euflauzino@gmail.com>
2024-11-26 16:52:33 +0000

    Update sort.c

--- Common_Core/push_swap/src/sort.c
+++ Common_Core/push_swap/src/sort.c
@@ -6,13 +6,13 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:44:19 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/11/26 16:04:55 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/11/26 16:48:47 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int     get_max(t_list **stack)
+int	get_max(t_list **stack)
 {
 	t_list	*tmp;
 	int		max;
@@ -36,11 +36,11 @@ void	callback(t_list **stack_a, t_list **stack_b)
 	}
 }
 
-int	byPosition(int num, int digits, int position)
+int	by_position(int num, int digits, int position)
 {
 	int	digit_position[digits];
 	int	i;
-	
+
 	i = digits;
 	while (i > 0)
 	{
@@ -49,15 +49,15 @@ int	byPosition(int num, int digits, int position)
 		i--;
 	}
 	return (digit_position[position]);
-}	
+}
 
 void	radix_sort(t_list **stack_a, t_list **stack_b)
 {
 	t_list	*head;
-	int	digits;
-	int	i;
-	int	j;
-	
+	int		digits;
+	int		i;
+	int		j;
+
 	digits = count_digits(get_max(stack_a));
 	i = 0;
 	while (i < digits)
@@ -68,7 +68,7 @@ void	radix_sort(t_list **stack_a, t_list **stack_b)
 			head = *stack_a;
 			while (head != NULL)
 			{
-				if (byPosition(head->content, digits, (digits - i - 1)) == j)
+				if (by_position(head->content, digits, (digits - i - 1)) == j)
 					pb(stack_a, stack_b);
 				head = head->next;
 			}
