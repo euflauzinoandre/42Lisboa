31b0a5df86d5b1f6e48360c6f3ea69eb176d8241
André Euflauzino <andre.euflauzino@gmail.com>
2024-11-26 16:41:27 +0000

    Update push_swap.c

--- Common_Core/push_swap/src/push_swap.c
+++ Common_Core/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aeuflauz <aeuflauz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:37:15 by aeuflauz          #+#    #+#             */
-/*   Updated: 2024/11/25 15:51:07 by aeuflauz         ###   ########.fr       */
+/*   Updated: 2024/11/26 13:52:44 by aeuflauz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ int	main(int argc, char **argv)
 	t_list	*stack_a;
 	t_list	*stack_b;
 
+	if (argc < 2)
+		return (-1);
 	stack_a = NULL;
 	stack_b = NULL;
 	init_stack(&stack_a, argc, argv);
-	print_stack("stack_a", stack_a);
-	print_stack("stack_b", stack_b);
+	radix_sort(&stack_a, &stack_b);
+	ft_printf("STACK_A SORTED: ");
+	print_stack(&stack_a);
 	free_stack(&stack_a);
 	free_stack(&stack_b);
 	return (0);
